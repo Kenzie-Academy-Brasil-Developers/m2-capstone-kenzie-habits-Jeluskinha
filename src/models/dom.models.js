9781676f7ba0ffb4cf28563fb1b81c862b983077
Jean Lucas <102565302+Jeluskinha@users.noreply.github.com>
2022-07-13 21:38:04 -0300

    feat: add modal create habit in dashboard.css

--- src/models/dom.models.js
+++ src/models/dom.models.js
@@ -1,60 +1,60 @@
-import Requests from "../controller/request.controller.js"
+// import Requests from "../controller/request.controller.js"
 
-export default class Habits{
+// export default class Habits{
 
-    static async adicionarLista(){
-        const lista = await Requests.readAll()
-        console.log(lista)
+//     static async adicionarLista(){
+//         const lista = await Requests.readAll()
+//         console.log(lista)
     
-        //selecionar html
-        const tabela = document.querySelector(".tabela__cabecalho")
+//         //selecionar html
+//         const tabela = document.querySelector(".tabela__cabecalho")
        
-        lista.forEach((item) => {
+//         lista.forEach((item) => {
     
-             //criar elementos lista de tarefas
-            const linhaTabela = document.createElement("tr")
-            const celulaCheckbox = document.createElement("td")
-            const checkbox = document.createElement("input")
-            const celulaTitulo = document.createElement("td")
-            const titulo = document.createElement("p")
-            const celulaDescricao = document.createElement("td")
-            const descricao = document.createElement("p")
-            const celulaCategoria = document.createElement("td")
-            const categoria = document.createElement("p")
-            const tabelaOpcoes = document.createElement("td")
-            const opcoes = document.createElement("button")
+//              //criar elementos lista de tarefas
+//             const linhaTabela = document.createElement("tr")
+//             const celulaCheckbox = document.createElement("td")
+//             const checkbox = document.createElement("input")
+//             const celulaTitulo = document.createElement("td")
+//             const titulo = document.createElement("p")
+//             const celulaDescricao = document.createElement("td")
+//             const descricao = document.createElement("p")
+//             const celulaCategoria = document.createElement("td")
+//             const categoria = document.createElement("p")
+//             const tabelaOpcoes = document.createElement("td")
+//             const opcoes = document.createElement("button")
     
     
-            //adicionar classes
-            linhaTabela.classList.add("tarefa")
-            celulaCheckbox.classList.add("tarefa__cumprido--checkbox")
-            checkbox.classList.add("tarefa__cumprido--check")
-            celulaTitulo.classList.add("tarefa__titulo")
-            titulo.classList.add("tarefa__titulo--texto")
-            celulaDescricao.classList.add("tarefa__descricao")
-            descricao.classList.add("tarefa__descricao--texto")
-            celulaCategoria.classList.add("tarefa__categoria")
-            categoria.classList.add("tarefa__categoria--box")
-            tabelaOpcoes.classList.add("tarefa__opcoes")
-            opcoes.classList.add("tarefa__opcoes--botao")
+//             //adicionar classes
+//             linhaTabela.classList.add("tarefa")
+//             celulaCheckbox.classList.add("tarefa__cumprido--checkbox")
+//             checkbox.classList.add("tarefa__cumprido--check")
+//             celulaTitulo.classList.add("tarefa__titulo")
+//             titulo.classList.add("tarefa__titulo--texto")
+//             celulaDescricao.classList.add("tarefa__descricao")
+//             descricao.classList.add("tarefa__descricao--texto")
+//             celulaCategoria.classList.add("tarefa__categoria")
+//             categoria.classList.add("tarefa__categoria--box")
+//             tabelaOpcoes.classList.add("tarefa__opcoes")
+//             opcoes.classList.add("tarefa__opcoes--botao")
         
-            titulo.innerText = ""
-            descricao.innerText = ""
-            categoria.innerText = ""
-            opcoes.innerHTML = "..."
+//             titulo.innerText = ""
+//             descricao.innerText = ""
+//             categoria.innerText = ""
+//             opcoes.innerHTML = "..."
         
-            tabelaOpcoes.append(opcoes)
-            celulaCategoria.append(categoria)
-            celulaDescricao.append(descricao)
-            celulaTitulo.append(titulo)
-            celulaCheckbox.append(checkbox)
-            linhaTabela.append(tabelaOpcoes,
-                        celulaCategoria,
-                        celulaDescricao,
-                        celulaTitulo,
-                        celulaCheckbox)
-            tabela.append(linhaTabela)
+//             tabelaOpcoes.append(opcoes)
+//             celulaCategoria.append(categoria)
+//             celulaDescricao.append(descricao)
+//             celulaTitulo.append(titulo)
+//             celulaCheckbox.append(checkbox)
+//             linhaTabela.append(tabelaOpcoes,
+//                         celulaCategoria,
+//                         celulaDescricao,
+//                         celulaTitulo,
+//                         celulaCheckbox)
+//             tabela.append(linhaTabela)
         
-        })
-     }
-}
+//         })
+//      }
+// }
