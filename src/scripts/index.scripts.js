6a7c90b2e21e50ff2584ecd46cee6a9d3879f06e
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2022-07-14 02:46:23 -0300

    feat: add erro alert to access danied

--- src/scripts/index.scripts.js
+++ src/scripts/index.scripts.js
@@ -3,20 +3,21 @@ import Requests from "../controller/request.controller.js"
 const form = document.querySelector("form")
 
 
-
 async function login(e){
     e.preventDefault()
     
     const fData                =  new FormData(e.target)
     const formDataObj          =  {}
     fData.forEach((value, key) => (formDataObj[key] = value))
-
     
     await Requests.login(formDataObj)
 
     const responseResultado = await Requests.login(formDataObj)
-    
-    window.location.href = "./src/views/dashboard.views.html"
+    if(responseResultado===undefined){
+        console.error("ACESSO NEGADO")
+        const alert = document.querySelector(".closebtn")
+        alert.parentElement.style.display = "unset"
+    } else{window.location.href = "./src/views/dashboard.views.html"}
 }
 form.addEventListener('submit', login)
 
