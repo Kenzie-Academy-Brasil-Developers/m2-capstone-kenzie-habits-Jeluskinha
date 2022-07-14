e9ec6436743e3bf61c7bf1264ab0cdbe9055a5c2
Victor Ávila <victoravila@Victors-MacBook-Pro.local>
2022-07-13 21:50:41 -0300

    feat: add model filtros

---
+++ src/models/filtros.models.js
@@ -0,0 +1,24 @@
+export default class Filtros{
+
+    static filtrarTodos(){
+
+        const divTodos = document.getElementById("todos")
+        divTodos.addEventListener("click", (evt) => {
+            console.log(evt.target);
+        })
+    }
+
+    static filtrarConcluidos(){
+
+        const divConcluido = document.getElementById("concluido")
+        divConcluido.addEventListener("click", (evt) => {
+            console.log(evt.target);
+        })
+    }
+
+    static atualizarCheckbox(){
+
+        // const divConcluido = document.querySelector(".__boxButtons--Titulo--modelo1")
+
+    }
+}
